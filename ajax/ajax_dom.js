df7484339c56571f7b67003140876ca5ecbcb15e
mancoxu <mancoxu@foxmail.com>
2016-12-13 13:19:26 +0800

    增加 ajax

---
+++ ajax/ajax_dom.js
@@ -0,0 +1,140 @@
+/**
+ * 
+ * Request
+ *   ajax,jsonp
+ * 
+ * **/
+;(function(){
+function minMaxRandom(under,over){
+	switch(arguments.length){
+		case 1: return parseInt(Math.random()*under);
+		case 2: return (parseInt(Math.random()*(over-under+1))+parseInt(under));
+		default:return 0;
+	};
+}
+
+function Request() {
+};
+Request.prototype = {
+	ajax : function(args) {
+		this.options = {
+			type : 'GET',
+			dataType : 'text',
+			async : true,
+			avatar : null,
+			contentType : 'application/x-www-form-urlencoded',
+			url : 'about:blank',
+			data : {},
+			success : {},
+			error : {}
+		};
+		if (!args) {
+			console.error('please fill in any parameters first!');
+			return;
+		} else if (!args.url) {
+			console.error('url is required parameters, please check your parameters!');
+			return;
+		} else if (!args.success || typeof args.success != 'function') {
+			console.error('the callback function is lost!');
+			return;
+		}
+		this.shift(this.options, args);
+		this.send();
+	},
+	jsonp : function(args) {
+		this.options = {
+			type : 'JSONP',
+			jsonpName : '',
+			dataType : 'text',
+			async : true,
+			avatar : null,
+			url : 'about:blank',
+			success : function(){},
+			data : {}
+		};
+		if (!args) {
+			console.error('please fill in any parameters first!');
+			return;
+		} else if (!args.url) {
+			console.error('url is required parameters, please check your parameters!');
+			return;
+		} else if (!args.jsonpName) {
+			args.jsonpName = 'jsonpCallbackFunctionNo' + new Date().getTime() + minMaxRandom(0, 9999);
+		}
+
+		this.shift(this.options, args);
+		if (window[this.options.jsonpName] && window[this.options.jsonpName] !== this.options.success) {
+			console.error('jsonpName already exists!');
+			return;
+		}
+		window[this.options.jsonpName] = this.options.success;
+		this.create();
+	},
+	create : function() {
+		var script = document.createElement('script'), argStr = /[\?]/g.test(this.options.url) ? '&' : '?';
+		for (var key in this.options.data) {
+			argStr += key + '=' + this.options.data[key] + '&';
+		}
+		argStr = argStr + 'callback=' + this.options.jsonpName;
+		script.async = this.options.async;
+		script.src = this.options.url + (argStr == '?' ? '' : argStr);
+		document.getElementsByTagName('head')[0].appendChild(script);
+	},
+	XmlHttp : function() {
+		var xmlHttp;
+		try {
+			xmlhttp = new XMLHttpRequest();
+		} catch(e) {
+			try {
+				xmlhttp = new ActiveXObject("Msxml2.XMLHTTP");
+			} catch(e) {
+				xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+			}
+		}
+		if (!xmlhttp) {
+			return false;
+		}
+		return xmlhttp;
+	},
+	send : function() {
+		var xmlHttp = this.XmlHttp(), linkSign = /[\?]/g.test(this.options.url) ? '&' : '?', argStr = '', _this = this, length = this.options.data ? this.options.data.length : 0;
+		for (var key in this.options.data) {
+			argStr += key + '=' + this.options.data[key] + '&';
+		}
+		argStr = argStr.replace(/\&$/g, '');
+		if (this.options.type.toUpperCase() == 'GET') {
+			xmlHttp.open(this.options.type, this.options.url + (argStr == '' ? '' : linkSign + argStr), this.options.async);
+		} else {
+			xmlHttp.open(this.options.type, this.options.url, this.options.async);
+		}
+		xmlHttp.setRequestHeader('Content-Type', this.options.contentType);
+		xmlHttp.onreadystatechange = function() {
+			if (xmlHttp.readyState == 4) {
+				if (xmlHttp.status == 200 || xmlHttp.status == 0) {
+					if ( typeof _this.options.success == 'function') {
+						var responseData = _this.options.dataType == 'text' ? xmlHttp.responseText : xmlHttp.responseXML;
+						_this.options.success(responseData, _this.options.dataType,_this.options.avatar);
+					}
+					xmlHttp = null;
+				} else {
+					if ( typeof _this.options.error == 'function')
+						_this.options.error('Server Status: ' + xmlHttp.status);
+				}
+			}
+		};
+		xmlHttp.send(this.options.type.toUpperCase() == 'POST' ? argStr : null);
+	},
+	shift : function(o, args) {
+		for (var i in args) {
+			o[i] = args[i];
+		};
+		return o;
+	}
+}; 
+
+	
+ window['Request'] = function(){
+ 	return new Request();
+ };
+
+})();
